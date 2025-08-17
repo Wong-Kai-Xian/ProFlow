55334e9771c247faca1c85666de581b337c4652f
Wong-Kai-Xian <94754367+Wong-Kai-Xian@users.noreply.github.com>
2025-08-17 22:57:43 +0800

    Fix syntax errors by removing all diff markers

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
-@@ .. @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "firebase/app";
- import { getAnalytics } from "firebase/analytics";
-+import { getFirestore } from "firebase/firestore";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
- 
-@@ .. @@
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
-+export const db = getFirestore(app);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v9-compat and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDTuJ5a3JHuKxOuFiMa1HGK7-9Qw2VxYzE",
+  authDomain: "proflow-b5e23.firebaseapp.com",
+  projectId: "proflow-b5e23",
+  storageBucket: "proflow-b5e23.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef1234567890abcdef",
+  measurementId: "G-ABCDEFGHIJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-@@ .. @@
- // src/index.js
- import React from "react";
- import ReactDOM from "react-dom/client";
-+import { BrowserRouter } from "react-router-dom";
- import Home from "./pages/Home";
- 
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(
-   <React.StrictMode>
--    <Home />
-+    <BrowserRouter>
-+      <Home />
-+    </BrowserRouter>
-   </React.StrictMode>
- );
+// src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  </React.StrictMode>
+);
