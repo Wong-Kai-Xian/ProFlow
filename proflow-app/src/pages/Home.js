040ada8764653e0d7d3b0a4cfc74b1c195b67119
Wong-Kai-Xian <94754367+Wong-Kai-Xian@users.noreply.github.com>
2025-08-17 22:20:44 +0800

    Create Home.js

---
+++ proflow-app/src/pages/Home.js
@@ -0,0 +1,38 @@
+// src/pages/Home.js
+import React from "react";
+import TopBar from "../components/TopBar";
+import Dashboard from "../components/Dashboard";
+import GroupForum from "../components/GroupForum";
+import ClientChat from "../components/ClientChat";
+import UpcomingEvents from "../components/UpcomingEvents";
+
+export default function Home() {
+  return (
+    <div>
+      <TopBar />
+      <div style={{
+        display: "flex",
+        marginTop: "20px",
+        padding: "10px",
+        gap: "20px",
+        minHeight: "90vh"
+      }}>
+        {/* Left Column */}
+        <div style={{ flex: 1, display: "flex", flexDirection: "column", gap: "20px" }}>
+          <GroupForum />
+          <ClientChat />
+        </div>
+
+        {/* Middle Column */}
+        <div style={{ flex: 2 }}>
+          <Dashboard />
+        </div>
+
+        {/* Right Column */}
+        <div style={{ flex: 1 }}>
+          <UpcomingEvents />
+        </div>
+      </div>
+    </div>
+  );
+}
