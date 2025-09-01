ed198caffaa9ef3f1a86722fa351b8286c8d169a
Wong-Kai-Xian <94754367+Wong-Kai-Xian@users.noreply.github.com>
2025-09-01 18:44:03 +0800

    remove api key

--- src/firebase.js
+++ src/firebase.js
@@ -9,14 +9,13 @@ import { getStorage } from "firebase/storage"; // Import getStorage
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCsYuQCsYXXvRUnFySseC8UNzArjCdJGSo",
-  authDomain: "proflow-30144.firebaseapp.com",
-  projectId: "proflow-30144",
-  // The storage bucket should be the bucket name (appspot.com)
-  storageBucket: "proflow-30144.firebasestorage.app",
-  messagingSenderId: "278194395988",
-  appId: "1:278194395988:web:560cf31ebb0f83f5f686c1",
-  measurementId: "G-RVY0MN3GWN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
